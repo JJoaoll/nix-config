e0e9cb79e5894adceb4867943415ce1cccdb68b9
JJoaoll <joaoduos@gmail.com>
2025-07-28 21:32:12 -0300

    polybar adjustment

--- nixos/i3/default.nix
+++ nixos/i3/default.nix
@@ -11,6 +11,13 @@
     xserver = {
       enable = true;
       videoDrivers = [ "nvidia" ];
+
+      monitorSection = ''
+        Option "PreferredMode" "1920x1080_360.00"
+      '';
+      screenSection = ''
+        Option "DPMS" "false"
+      '';
       # TODO: tablet
 
       # desktioManager.plasma6.enable = false;
@@ -19,6 +26,10 @@
         enable = true;
         greeters.gtk.enable = true;
       };
+
+      displayManager.sessionCommands = ''
+        xrandr --output DP-0 --mode 1920x1080 --rate 360
+      '';
 
       windowManager.i3 = {
         enable = true;

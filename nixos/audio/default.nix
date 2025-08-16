3b1da46489b2de568b3c4400f26647ccd54d8f77
JJoaoll <joaoduos@gmail.com>
2025-08-16 12:10:21 -0300

    del: xmonad

---
+++ nixos/audio/default.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }: {
+
+#TODO: DO! O audio nao ta integrado com nd!
+
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+      jack.enable = true;
+
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+  
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+
+  users.users.jjoaoll.extraGroups = [ "audio" "pipewire" "realtime" ];
+
+  environment.systemPackages = with pkgs; [
+    pamixer       
+    pulseaudio-ctl
+    wireplumber   
+    pavucontrol 
+
+    easyeffects
+
+    rnnoise
+    rnnoise-plugin
+    speech-denoiser
+
+    calf
+    lsp-plugins
+
+    adwaita-icon-theme
+    xdg-desktop-portal-gtk
+  ];
+
+
+
+
+
+
+
+}

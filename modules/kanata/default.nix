3e658ed4d0994e054834e68ad6326d6d91cecae6
JJoaoll <joaoduos@gmail.com>
2026-01-01 18:14:48 -0300

    ✅ zellij 1/2
    works with "zellij a -c main" sessions but a version with environment
    variables still missing

--- modules/kanata/default.nix
+++ modules/kanata/default.nix
@@ -1,33 +1,4 @@
 { config, pkgs, lib, ... }: 
-# let 
-#   gen = import ./xcompose/gen.nix;
-#   emoji_file = import ./xcompose/emoji.nix;
-#   emoji_keys = gen {keymaps = emoji_file; inherit lib;};
-#   myUser = "jjoaoll";
-#   unicodeComposeFiles = [
-#     # ./.unicodef/emoji.XCompose
-#     ./.unicodef/fonts.XCompose
-#     ./.unicodef/games.XCompose
-#     ./.unicodef/greek.XCompose
-#     ./.unicodef/lang.XCompose
-#     ./.unicodef/math.XCompose
-#     ./.unicodef/misc.XCompose
-#     ./.unicodef/thatex.XCompose
-#   ];
-#
-#   combinedUnicodeComposeContent = lib.concatLines (
-#     lib.map builtins.readFile unicodeComposeFiles
-#   ) + emoji_keys;
-#
-#   baseXComposeContent = ''
-#     include "%L"
-#   '';
-#
-#   finalXComposeContent = "${baseXComposeContent}\n${combinedUnicodeComposeContent}";
-#
-#   combinedXComposeFile = pkgs.writeText "my-combined-xcompose" finalXComposeContent;
-#
-# in {
 {
 
   # Configure keymap in X11
@@ -39,11 +10,21 @@
 
   services.kanata = {
     enable = true;
+
+    # TODO: UNSAFE UNSAFE UNSAFE
+    package = pkgs.kanata.override { withCmd = true; };
+
     keyboards.my-keyboard = {
       devices = [ 
         "/dev/input/by-path/pci-0000:07:00.3-usb-0:4:1.0-event-kbd" 
       ];
-      config = builtins.readFile ./layout.kbd;
+
+    extraDefCfg = ''
+          process-unmapped-keys yes
+          danger-enable-cmd yes
+
+    '';
+    config = builtins.readFile ./layout.kbd;
       #TODO: configFile = 
     };
   };
